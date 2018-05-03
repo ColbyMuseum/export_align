422a7f13d01e0e56da8863fdc038411810e50de5
charlie butcosk <cbutcosk@users.noreply.github.com>
2018-05-03 16:53:47 -0400

    Fix jena install URL

--- install_riot.sh
+++ install_riot.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-curl -O http://ftp.wayne.edu/apache/jena/binaries/apache-jena-3.6.0.zip
+curl -O http://archive.apache.org/dist/jena/binaries/apache-jena-3.6.0.zip
 mkdir -p bin
 unzip apache-jena-3.6.0.zip -d bin/
